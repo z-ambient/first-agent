7f47160e930dd2ec6be76f94bebcd5bf50d6aa56
z-ambient <zdoss330@gmail.com>
2026-02-16 15:21:52 -0500

    added verbose

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -14,16 +15,27 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true",
+                    help="Enable verbose output")
 args = parser.parse_args()
 
+messages = [types.Content(
+    role="user", parts=[types.Part(text=args.user_prompt)])]
+
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=args.user_prompt
+    model='gemini-2.5-flash', contents=messages
 )
 
-usage = response.usage_metadata
 
+if args.verbose:
+    usage = response.usage_metadata
+    if usage is None:
+        raise RuntimeError("No usage")
 
-print(f"User prompt: {args.user_prompt}")
-print(f"Prompt tokens: {usage.prompt_token_count}")
-print(f"Response tokens: {usage.candidates_token_count}")
-print(f"Response:\n{response.text}")
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {usage.prompt_token_count}")
+    print(f"Response tokens: {usage.candidates_token_count}")
+    print(f"Response:\n{response.text}")
+
+else:
+    print(f"Response:\n{response.text}")
